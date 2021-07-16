2dd9906b7b8d7a48199af807a1c7060cd9dd9e50
Kerry Lang <klang812@gmail.com>
2021-07-16 11:01:51 -0700

    add productDenied each loop to catch added products with approvals

--- cli.rb
+++ cli.rb
@@ -1,34 +1,26 @@
 #!/usr/bin/env ruby
 
-require 'optparse'
+# require 'optparse'
 
-options = {}
+# options = {}
 
-OptionParser.new do |parser|
-  parser.on("-fFILE", "--file=FILE", "The sample-input file") do |value|
-    options[:file] = value
-  end
-end.parse!
+# OptionParser.new do |parser|
+#   parser.on("-fFILE", "--file=FILE", "The sample-input file") do |value|
+#     options[:file] = value
+#   end
+# end.parse!
 
-
-# file = File.open(options[:file])
-# puts file.read 
-
-
-# file.close
-
-
-# puts "Hello " + options[:name] + "!"
-
-file_data = File.read(options[:file]).split("\n")
+file_data = File.read(ARGV[0]).split("\n")
 
 productApproval = []
 
 file_data.each do |entry| 
+  # i.e. entry = "G.C., keychain - approve"
   dataEntry = entry.split(" ")
+  # dataEntry = ["G.C.,","keychain","-","approve"]
   product = ""
   approval = dataEntry[-1]
-  # for (i=1; i<=entry.length[-3]; i++) do
+  # i.e. approval = "approve"
   i=1
   while i<=(dataEntry.length() -3) do
     product += dataEntry[i] + " "
@@ -36,21 +28,27 @@ file_data.each do |entry|
   end
   productApproval.push(product.strip => approval)
 end
-# puts productApproval
-# mug => approved
+
 productApproved = []
 productDenied = []
 
 productApproval.each do |approval|
-  # puts approval.keys[0]
-  # puts approval.values[0]
   if (approval.values[0] === 'approve')
     productApproved.push(approval.keys[0])
   else 
     productDenied.push(approval.keys[0])
-    productApproved.delete(approval.keys[0])
+    # productApproved.delete(approval.keys[0]) (initial approach - if last element was in approval it would also add to approval list)
   end
 end
+
+# if product is not 100% approved it will be rejected (based on requirements)
+productDenied.each do |denied|
+  # denied = keychain
+  if (productApproved.include? denied)
+    productApproved.delete(denied)
+  end
+end
+
 puts "Approved"
 puts productApproved.sort.uniq
 puts
