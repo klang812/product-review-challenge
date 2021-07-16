244fa76ae5e7350190bb92e6dd0cf1ca1f1e00e2
Kerry Lang <klang812@gmail.com>
2021-07-16 10:10:51 -0700

    working code

--- cli.rb
+++ cli.rb
@@ -5,10 +5,54 @@ require 'optparse'
 options = {}
 
 OptionParser.new do |parser|
-  parser.on("-n", "--name", "The name of the reviewer") do |value|
-    options[:name] = value
+  parser.on("-fFILE", "--file=FILE", "The sample-input file") do |value|
+    options[:file] = value
   end
 end.parse!
 
-puts "Hello " + options[:name] + "!"
 
+# file = File.open(options[:file])
+# puts file.read 
+
+
+# file.close
+
+
+# puts "Hello " + options[:name] + "!"
+
+file_data = File.read(options[:file]).split("\n")
+
+productApproval = []
+
+file_data.each do |entry| 
+  dataEntry = entry.split(" ")
+  product = ""
+  approval = dataEntry[-1]
+  # for (i=1; i<=entry.length[-3]; i++) do
+  i=1
+  while i<=(dataEntry.length() -3) do
+    product += dataEntry[i] + " "
+    i += 1
+  end
+  productApproval.push(product.strip => approval)
+end
+# puts productApproval
+# mug => approved
+productApproved = []
+productDenied = []
+
+productApproval.each do |approval|
+  # puts approval.keys[0]
+  # puts approval.values[0]
+  if (approval.values[0] === 'approve')
+    productApproved.push(approval.keys[0])
+  else 
+    productDenied.push(approval.keys[0])
+    productApproved.delete(approval.keys[0])
+  end
+end
+puts "Approved"
+puts productApproved.sort.uniq
+puts
+puts "Denied"
+puts productDenied.sort.uniq
--- reviews.rb
+++ reviews.rb
@@ -1,14 +1,14 @@
-puts "What is your name?"
-name = gets
+# puts "What is your name?"
+# name = gets.chomp
 
-puts "What is the product you would like to review?"
-product = gets
+# puts "What is the product you would like to review?"
+# product = gets.chomp
 
-puts "What is your review of the product? (Approve or Reject)"
-review = gets
+# puts "What is your review of the product? (Approve or Reject)"
+# review = gets.chomp
 
-if review === "Approve" || "approve"
-  puts "Thank you! #{product} is approved!"
-else
-  puts "Thank you! #{product} is denied!"
-end
+# if review == "Approve" || "approve"
+#   puts "Thank you! #{product} is approved!"
+# else
+#   puts "Thank you! #{product} is denied!"
+# end
