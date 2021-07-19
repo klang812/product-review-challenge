a1084cea4fd65101615c1605c21805cee81cb3ab
Kerry Lang <klang812@gmail.com>
2021-07-18 17:52:59 -0700

    move functions to a class

--- cli.rb
+++ cli.rb
@@ -9,71 +9,72 @@
 #     options[:file] = value
 #   end
 # end.parse!
+class ProductReviews
+  
+  def initialize
+    @productApproval = []
+    @productApproved = []
+    @productDenied = []
+  end
+  
+  def load_file(fileName)
+    File.read(fileName).split("\n")
+  end
 
-$productApproval = []
-$productApproved = []
-$productDenied = []
-
-def load_file(fileName)
-  File.read(fileName).split("\n")
-end
-
-def read_file
-  file_data = load_file(ARGV[0])
-  file_data.each do |entry| 
-    # i.e. entry = "G.C., keychain - approve"
-    dataEntry = entry.split(" ")
-    # dataEntry = ["G.C.,","keychain","-","approve"]
-    product = ""
-    approval = dataEntry[-1]
-    # i.e. approval = "approve"
-    i=1
-    while i<=(dataEntry.length() -3) do
-      product += dataEntry[i] + " "
-      i += 1
+  def read_file(fileName)
+    file_data = load_file(fileName)
+    file_data.each do |entry| 
+      # i.e. entry = "G.C., keychain - approve"
+      dataEntry = entry.split(" ")
+      # dataEntry = ["G.C.,","keychain","-","approve"]
+      product = ""
+      approval = dataEntry[-1]
+      # i.e. approval = "approve"
+      i=1
+      while i<=(dataEntry.length() -3) do
+        product += dataEntry[i] + " "
+        i += 1
+      end
+      @productApproval.push(product.strip => approval)
     end
-    $productApproval.push(product.strip => approval)
+  end
+
+  def populate_approve_and_deny_lists
+    @productApproval.each do |approval|
+      if (approval.values[0] === 'approve')
+        @productApproved.push(approval.keys[0])
+      else 
+        @productDenied.push(approval.keys[0])
+        # $@productApproved.delete(approval.keys[0]) # (initial approach - if last element was in approval it would also add to approval list)
+      end
+    end
+  end
+
+  def remove_product_from_approval_if_denied
+    # if product is not 100% approved it will be rejected (based on requirements)
+    @productDenied.each do |denied|
+      # denied = keychain
+      if (@productApproved.include? denied)
+        @productApproved.delete(denied)
+      end
+    end
+  end
+
+  def display_results
+    puts "Approved"
+    puts @productApproved.sort.uniq
+    puts
+    puts "Denied"
+    puts @productDenied.sort.uniq
+  end
+
+  def run_application(fileName)
+    read_file(fileName)
+    populate_approve_and_deny_lists()
+    remove_product_from_approval_if_denied()
+    display_results()
   end
 end
 
-def populate_approve_and_deny_lists
-  $productApproval.each do |approval|
-    if (approval.values[0] === 'approve')
-      $productApproved.push(approval.keys[0])
-    else 
-      $productDenied.push(approval.keys[0])
-      # $productApproved.delete(approval.keys[0]) # (initial approach - if last element was in approval it would also add to approval list)
-    end
-  end
-end
-
-def remove_product_from_approval_if_denied
-  # if product is not 100% approved it will be rejected (based on requirements)
-  $productDenied.each do |denied|
-    # denied = keychain
-    if ($productApproved.include? denied)
-      $productApproved.delete(denied)
-    end
-  end
-end
-
-def display_results
-  puts "Approved"
-  puts $productApproved.sort.uniq
-  puts
-  puts "Denied"
-  puts $productDenied.sort.uniq
-end
-
-
-
-
-read_file()
-
-
-
-populate_approve_and_deny_lists()
-
-remove_product_from_approval_if_denied()
-
-display_results()
+product_reviews = ProductReviews.new
+product_reviews.run_application(ARGV[0])
